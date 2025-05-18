fa2d76d40dfe66a7eeb91e54939c03e2992563e5
KaiGosling <murcillo.kyleemmanuel.cervantes@gmail.com>
2025-05-18 21:48:30 +0800

    start kiosk

---
+++ 0.0.7/start folder/startmainkisok.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="start.css">
+  <title>PC Worth Kiosk</title>
+
+</head>
+<body>
+// ETO YUNG PANG BACKGROUND //
+<div class="slideshow">
+  <div class="slide" style="background-image: url('img1.png');"></div>
+  <div class="slide" style="background-image: url('img2.png');"></div>
+  <div class="slide" style="background-image: url('img3.png');"></div>
+</div>
+
+
+  <div>
+  <button onclick="location.href='secmainkiosk.php'" class="start-button">Tap to Start </button>
+  
+  </div>
+  <div class="buttomheader">
+  <img class="pbsbi" src="pbsbi.png" alt="kiosk bottom header">
+
+  
+
+ 
+</div>
+</body>
+</html>
