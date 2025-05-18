c5706ab4736700e39c881c73c84dd17e7d9ce966
KaiGosling <murcillo.kyleemmanuel.cervantes@gmail.com>
2025-05-19 00:20:20 +0800

    jabol

--- start folder/secmainkiosk.php
+++ start folder/secmainkiosk.php
@@ -1,11 +1,43 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <title>My PHP Website</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="secmain.css">
+  <title>PC Worth - Landing Page</title>
+ 
 </head>
 <body>
+  <div class="logo">
+<img class="kiosklog" src="headstart.png" alt="PC Worth Logo"> 
+  </div>
+<br>
+  <div class="menu-section">
+     <a href ="prebuilds.html" >
 
-<h1><?php echo "Hello, World!"; ?></h1>
+   <div class="menu-button">  <h2>Pre-Build</h2>
+      <img src="imt1.png" alt="Pre-Build"> 
+    
+    </div>
+    </a>
+    <a href ="custombuild.html" >
+    <div class="menu-button">   <br>  <h2>Custom Build</h2>
+      <img src="imt2.png" alt="Custom Build">
+
+    </div>
+    </a>
+
+
+    <a href ="partsout.html" >
+
+    <div class="menu-button"><br> <h2>Parts Out</h2>
+      <img class="tribt" src="imt3.png" alt="Parts Out">
+      
+    </div>
+    </a>
+  </div>
+  
+
 
 </body>
 </html>
