c48d80146e30d882097dab273d50995e4cc78246
rebpogi <ravedomecillo@gmail.com>
2025-06-07 22:41:29 +0800

    test

--- Admin_Zas/0.0.15 working/admin.php
+++ Admin_Zas/0.0.15 working/admin.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Dashboard admin</title>
+  <title>PC Worth Dashboard admin</title>
 
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
     integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
